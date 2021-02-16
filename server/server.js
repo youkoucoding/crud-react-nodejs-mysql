4b5fb145a2931b38c3b2ba225f2cb3442005e189
youkoucoding <youkousai@gmail.com>
2021-02-17 02:47:31 +0900

    add insertSQL

--- server/server.js
+++ server/server.js
@@ -16,11 +16,26 @@ const db = mysql.createPool({
   database: process.env.DATABASE
 });
 
-app.get('/', (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  const insertSql = 'INSERT INTO movie_reviews (movieName, movieReview) VALUES ("Harry Potter","Excellent!");';
-  db.query(insertSql, (err, result) => {
-    res.json({ "title": "hello CRUD" });
+app.get('/api/get', (req, res) => {
+  const sqlSelect = "SELECT * FROM movie_reviews";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+
+});
+
+app.post('/api/insert ', (req, res) => {
+
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
+  const insertSql = 'INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?);';
+  db.query(insertSql, [movieName, movieReview], (err, result) => {
+    console.log(result);
   });
 });
 
